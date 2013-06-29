97f743b9afef6c1d4d6ecd6cf351fb42e9bbe669
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 13:28:14 -0400

    Basic piping from filesystem to browser.

---
+++ 1.5.3.piping.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var http = require('http');
+var server = http.createServer();
+
+// Cloud9: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts
+process.env.PORT = process.env.PORT? process.env.PORT : "3000";
+process.env.IP   = process.env.IP? process.env.IP : "127.0.0.1";
+
+server.on('request', function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.createReadStream('./lionpta.png').pipe(res);
+});
+
+server.listen(process.env.PORT);
+
+console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT + "/");
