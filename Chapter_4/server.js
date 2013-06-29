e3eb00c8c00d3a742d88c5f88e9516d5c5542915
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 18:38:28 -0400

    Page 102

--- Chapter_4/server.js
+++ Chapter_4/server.js
@@ -4,7 +4,7 @@ var server  = http.createServer();
 // Cloud9: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts
 process.env.PORT = process.env.PORT? process.env.PORT : "3000";
 process.env.IP   = process.env.IP? process.env.IP : "127.0.0.1";
-
+/*
 server.on('request', function(request, response){
 // Other fields/methods on response
 //    response.setHeader('Content-Length', body.length);
@@ -20,7 +20,15 @@ server.on('request', function(request, response){
 //    response.write("Hello World\n");
 //    response.end();
 });
-
+*/
+server.on('request', function(request, response){
+    request.setEncoding('utf8');
+    request.on('data', function(chunk){ console.log('parsed', chunk)});
+    request.on('end', function(){
+        console.log('done parsing');
+        response.end();
+    });
+});
 server.listen(process.env.PORT, function() {
     console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT + "/");
 });
