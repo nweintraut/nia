88d874d0e2d5c4f2c92c6fc92340bb3c17173c68
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 18:52:54 -0400

    Page 108

--- Chapter_4/server.js
+++ Chapter_4/server.js
@@ -1,5 +1,6 @@
 var http    = require('http');
 var server  = http.createServer();
+var url     = require('url');
 
 // Cloud9: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts
 process.env.PORT = process.env.PORT? process.env.PORT : "3000";
@@ -21,13 +22,42 @@ server.on('request', function(request, response){
 //    response.end();
 });
 */
+var items = [];
 server.on('request', function(request, response){
-    request.setEncoding('utf8');
-    request.on('data', function(chunk){ console.log('parsed', chunk)});
-    request.on('end', function(){
-        console.log('done parsing');
-        response.end();
-    });
+    switch (request.method) {
+        case 'POST': 
+            var item = '';
+            request.setEncoding('utf8');
+            request.on('data', function(chunk){ item += chunk;});
+            request.on('end', function(){
+                items.push(item);
+                response.end('OK\n');
+            });
+            
+            break;
+        case 'GET':
+            response.setHeader('Content-Type', 'text/plain');
+            response.statusCode = 200;
+            items.forEach(function(item, i){
+                response.write(i + ') ' + item + '\n');
+            });
+            response.end();
+            break;
+        case 'DELETE' : 
+            var path = url.parse(request.url).pathname;
+            var i = parseInt(path.slice(1), 10);
+            if (isNaN(i)) {
+                response.statusCode = 400;
+                response.end ('Invalid item id');
+            } else if (!items[i]) {
+                response.statusCode = 404;
+                response.end('Item not found');
+            } else {
+                items.splice(i, 1);
+                response.end('OK\n');
+            }
+    }
+
 });
 server.listen(process.env.PORT, function() {
     console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT + "/");
