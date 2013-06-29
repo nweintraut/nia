6dc904f5f01685c907911442aa0b71964b34cbf5
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 13:01:37 -0400

    Trying to resolve localhost not having a default PORT and Host enviromental variable

--- 1.5.2.server.js
+++ 1.5.2.server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 // Cloud9: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts
+process.env.PORT = process.env.PORT? process.env.PORT : "3000";
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plaim'});
     res.end('Hello World\n');
