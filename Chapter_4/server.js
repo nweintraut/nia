da00df6a4d557d91d56ba200bb69a1ce7692fe87
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 18:33:36 -0400

    Page 101.

--- Chapter_4/server.js
+++ Chapter_4/server.js
@@ -10,9 +10,15 @@ server.on('request', function(request, response){
 //    response.setHeader('Content-Length', body.length);
 //    response.setHeader('Content-Type', 'text/plain');
 //    response.statusCode = 302;
-    
-    response.write("Hello World\n");
-    response.end();
+    var url = 'http://google.com';
+    var body = '<p>Redirecting to <a href=\"' + url + '\">' + url + '</a></p>';
+    response.setHeader('Location', url);
+    response.setHeader('Content-Length', body.length);
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 302;
+    response.end(body);    
+//    response.write("Hello World\n");
+//    response.end();
 });
 
 server.listen(process.env.PORT, function() {
