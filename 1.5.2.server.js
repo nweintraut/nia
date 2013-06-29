429e077453c547e1517aceaf683987baee08b555
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 13:10:33 -0400

    Resilient for host and port. Note need to use URL scheume for Cloud9

--- 1.5.2.server.js
+++ 1.5.2.server.js
@@ -1,8 +1,9 @@
 var http = require('http');
 // Cloud9: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts
 process.env.PORT = process.env.PORT? process.env.PORT : "3000";
+process.env.IP   = process.env.IP? process.env.IP : "127.0.0.1"
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plaim'});
     res.end('Hello World\n');
 }).listen(process.env.PORT);
-console.log('Server running at ' + process.env.IP + ":" + process.env.PORT);
+console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT + "/");
