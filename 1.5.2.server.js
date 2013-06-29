8fefbdf2cceb7e4aa54f11409fe88542d8e720d4
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 13:15:07 -0400

    Corrected at typo.

--- 1.5.2.server.js
+++ 1.5.2.server.js
@@ -3,7 +3,7 @@ var http = require('http');
 process.env.PORT = process.env.PORT? process.env.PORT : "3000";
 process.env.IP   = process.env.IP? process.env.IP : "127.0.0.1"
 http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plaim'});
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 }).listen(process.env.PORT);
 console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT + "/");
