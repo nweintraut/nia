25f6835b60e66995f27159d36bf3ee411b36aa4d
Neil Weintraut <neil.weintraut@gmail.com>
2013-06-29 20:02:24 -0400

    Page 119

---
+++ Chapter_4/form.js
@@ -0,0 +1,107 @@
+var http    = require('http');
+var server  = http.createServer();
+var url     = require('url');
+var parse   = url.parse;
+var join    = require('path').join;
+var fs      = require('fs');
+var root    = __dirname;
+
+// Cloud9: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts
+process.env.PORT = process.env.PORT? process.env.PORT : "3000";
+process.env.IP   = process.env.IP? process.env.IP : "127.0.0.1";
+/*
+server.on('request', function(request, response){
+// Other fields/methods on response
+//    response.setHeader('Content-Length', body.length);
+//    response.setHeader('Content-Type', 'text/plain');
+//    response.statusCode = 302;
+    var url = 'http://google.com';
+    var body = '<p>Redirecting to <a href=\"' + url + '\">' + url + '</a></p>';
+    response.setHeader('Location', url);
+    response.setHeader('Content-Length', body.length);
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 302;
+    response.end(body);    
+//    response.write("Hello World\n");
+//    response.end();
+});
+*/
+/*
+var items = [];
+server.on('request', function(request, response){
+    switch (request.method) {
+        case 'POST': 
+            var item = '';
+            request.setEncoding('utf8');
+            request.on('data', function(chunk){ item += chunk;});
+            request.on('end', function(){
+                items.push(item);
+                response.end('OK\n');
+            });
+            
+            break;
+        case 'GET':
+            response.setHeader('Content-Type', 'text/plain');
+            response.statusCode = 200;
+            items.forEach(function(item, i){
+                response.write(i + ') ' + item + '\n');
+            });
+            response.end();
+            break;
+        case 'DELETE' : 
+            var path = url.parse(request.url).pathname;
+            var i = parseInt(path.slice(1), 10);
+            if (isNaN(i)) {
+                response.statusCode = 400;
+                response.end ('Invalid item id');
+            } else if (!items[i]) {
+                response.statusCode = 404;
+                response.end('Item not found');
+            } else {
+                items.splice(i, 1);
+                response.end('OK\n');
+            }
+    }
+});
+*/
+server.on('request', function(request, response){
+    var sourceUrl = parse(request.url);
+    var path = join(root, sourceUrl.pathname);
+    fs.stat(path, function(error, stat){
+        if (error) {
+            if('ENOENT' === error.code) {
+                response.statusCode = 404;
+                response.end('Not found');
+            } else {
+                response.statusCode = 500;
+                response.end('Internal Server Error');
+            }
+        } else {
+            response.setHeader('Content-Length', stat.size);
+            var stream = fs.createReadStream(path);
+            stream.on('eeor', function(err){
+                response.statusCode = 500;
+                response.end('Internal Server Error');
+            });
+            stream.pipe(response);
+        }
+    });
+    var stream = fs.createReadStream(path);
+
+    stream.on('error', function(err){
+        response.statusCode = 500;
+        response.end('Internal Server Error');
+    });
+    stream.pipe(response);    
+    /*
+    stream.on('data', function(chunk){
+        response.write(chunk);
+    });
+    stream.on('end', function(){
+       response.end(); 
+    });
+    */
+});
+server.listen(process.env.PORT, function() {
+    console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT + "/");
+});
--- Chapter_4/server.js
+++ Chapter_4/server.js
@@ -5,83 +5,67 @@ var parse   = url.parse;
 var join    = require('path').join;
 var fs      = require('fs');
 var root    = __dirname;
+var items   = [];
+var qs      = require('querystring');
 
 // Cloud9: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts
 process.env.PORT = process.env.PORT? process.env.PORT : "3000";
 process.env.IP   = process.env.IP? process.env.IP : "127.0.0.1";
-/*
-server.on('request', function(request, response){
-// Other fields/methods on response
-//    response.setHeader('Content-Length', body.length);
-//    response.setHeader('Content-Type', 'text/plain');
-//    response.statusCode = 302;
-    var url = 'http://google.com';
-    var body = '<p>Redirecting to <a href=\"' + url + '\">' + url + '</a></p>';
-    response.setHeader('Location', url);
-    response.setHeader('Content-Length', body.length);
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 302;
-    response.end(body);    
-//    response.write("Hello World\n");
-//    response.end();
-});
-*/
-/*
-var items = [];
-server.on('request', function(request, response){
-    switch (request.method) {
-        case 'POST': 
-            var item = '';
-            request.setEncoding('utf8');
-            request.on('data', function(chunk){ item += chunk;});
-            request.on('end', function(){
-                items.push(item);
-                response.end('OK\n');
-            });
-            
-            break;
-        case 'GET':
-            response.setHeader('Content-Type', 'text/plain');
-            response.statusCode = 200;
-            items.forEach(function(item, i){
-                response.write(i + ') ' + item + '\n');
-            });
-            response.end();
-            break;
-        case 'DELETE' : 
-            var path = url.parse(request.url).pathname;
-            var i = parseInt(path.slice(1), 10);
-            if (isNaN(i)) {
-                response.statusCode = 400;
-                response.end ('Invalid item id');
-            } else if (!items[i]) {
-                response.statusCode = 404;
-                response.end('Item not found');
-            } else {
-                items.splice(i, 1);
-                response.end('OK\n');
-            }
-    }
-});
-*/
-server.on('request', function(request, response){
-    var sourceUrl = parse(request.url);
-    var path = join(root, sourceUrl.pathname);
-    var stream = fs.createReadStream(path);
 
-    stream.on('error', function(err){
-        response.statusCode = 500;
-        response.end('Internal Server Error');
+function show(response){
+    var html = '<html><head><title>Todo List</title></head><body>';
+    html += '<h1>Todo List</h1></ul>';
+    html += items.map(function(item){
+        return '<li>' + item + '</li>';
+    }).join('');
+    html += '</ul>';
+    html += '<form method="post" action="/">';
+    html += '<p><input type="text" name="item" /></p>';
+    html += '<p><input type="submit" value="Add Item" /></p>';
+    html += '</form></body></html>';
+    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Content-Length', Buffer.byteLength(html));
+    response.end(html);
+}
+
+function notFound(response) {
+    response.statusCode = 404;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Not Found');
+}
+function badRequest(response) {
+    response.statusCode = 400;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Bad Request');
+}
+function add(request, response) {
+    var body = '';
+    request.setEncoding('utf8');
+    request.on('data', function(chunk){
+        body += chunk;
     });
-    stream.pipe(response);    
-    /*
-    stream.on('data', function(chunk){
-        response.write(chunk);
+    request.on('end', function(){
+        var obj = qs.parse(body);
+        items.push(obj.item);
+        show(response);
     });
-    stream.on('end', function(){
-       response.end(); 
-    });
-    */
+}
+server.on('request', function(request, response){
+    if ('/' === request.url) {
+        switch (request.method) {
+            case 'GET':
+                show(response);
+                break;
+            case 'POST':
+                add(request, response);
+                break;
+            default:
+                badRequest(response);
+        }
+    } else {
+        notFound(response);
+    }
+
 });
 server.listen(process.env.PORT, function() {
     console.log('Server running at http://' + process.env.IP + ":" + process.env.PORT + "/");
